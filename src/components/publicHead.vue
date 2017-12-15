2be0e45040e413a284e6cff34f489826a981660c
xiezg <xiezg@500wan.com>
2017-12-15 18:09:16 +0800

    '1215'

--- src/components/publicHead.vue
+++ src/components/publicHead.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="header_pop">
-        <span class="btn btn-back"  v-tap="{methods:goback}"></span>
+        <span class="btn btn-back" v-if=" !(this.personTitle ==='疯狂猜球-猜足球赢豪礼' && this.isCopyWeix ) "  v-tap="{methods:goback}"></span>
         <h1>{{ this.personTitle }}</h1>
         <a v-tap="{methods:jumpRight}" v-if="showFeedback" class="feedback-re">
             反馈记录
@@ -10,11 +10,13 @@
 </template>
 
 <script>
+    import { isWeiX } from '~common/util'
     export default {
         props: ['personTitle'],
         data () {
             return {
-                showFeedback: false
+                showFeedback: false,
+                isCopyWeix:null,
             }
         },
         methods: {
@@ -37,6 +39,8 @@
             if (this.personTitle === '我要反馈') {
                 this.showFeedback = true
             }
+            this.isCopyWeix = isWeiX ;
+            console.log( this.isCopyWeix );
         },
         computed: {
             userInfo () {
