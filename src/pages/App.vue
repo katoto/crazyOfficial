335b6129a651ade7b20a1eb2c525f5b6c53002fd
xiezg <xiezg@500wan.com>
2017-12-26 16:14:31 +0800

    '1226'

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -72,10 +72,10 @@
         /* 登陆还得修改下 */
             try {
             //    线下账号  测试  !!!
-                await this.$store.dispatch('localLogin', 12342)
-                await this.$store.dispatch('getUserInfo')
+//                await this.$store.dispatch('localLogin', 12342)
+//                await this.$store.dispatch('getUserInfo')
             /* 线上 登陆 */
-//                await this.$store.dispatch('checkLogin')
+                await this.$store.dispatch('checkLogin')
 
                 if (this.isLogin) {
                     await this.$store.dispatch('getUserInfo')
