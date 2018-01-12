54a776b7e392a214b9794de80c05f5c3afe78870
xiezg <xiezg@500wan.com>
2018-01-12 11:59:35 +0800

    '112'

--- src/pages/lotteryNotes.vue
+++ src/pages/lotteryNotes.vue
@@ -3,25 +3,28 @@
         <div class="respon2-itm text-left">
             <div class="full-scroll">
                 <PublicHead person-title="抽奖说明"></PublicHead>
-                <div class="intro_title">1.抽奖规则</div>
-                <div class="intro_text">
-                    您一但参与本活动即视为同意本规则。
-                    <br> 抽奖列表包含若干实物奖品，每个奖品抽奖所需的金币和实际抽取奖品可能不同；
-                    <br> 奖品能否被抽取到，跟您的选择有关；如果没能抽取到您心仪的商品，也有可能获得一定量的猜球币。
-                </div>
-                <div class="intro_title">2.奖品说明</div>
-                <div class="intro_text">
-                    请您在抽取商品后，尽快填写您的收货地址；1个月未能填写正确地址的，我们视为弃奖。
-                    <br> 因网络波动影响，部分奖品在抽取到后，未能及时发放；请您在网络稳定后前往奖品记录里核对中奖信息。
-                </div>
-                <div class="intro_title">3.其他声明</div>
-                <div class="intro_text">
-                    如检测到您恶意使用第三方工具或者脚本参与本活动，疯狂猜球运营团队有权回收您所有异常所得，情节严重者将永久封停账号。
-                </div>
-                <div class="intro_title">4.用户服务</div>
-                <div class="intro_text">
-                    如有任何疑问，请联系客服。
-                </div>
+                    <!-- 说明开始 -->
+                    <div class="intro_title">1.兑换规则</div>
+                    <div class="intro_text">
+                        您一但参与兑换即视为同意本规则。
+                        <br> 兑换使用的兑换券为虚拟道具，用户可通过幸运转盘和排行榜获得，兑换券可用于兑换玩家心仪的奖品。
+                        当充值获得额外赠送时需要消费满所需猜球币流水，即可恢复正常兑换。
+                        <br> 若遇到奖品缺货，我们将退还您的兑换券，具体操作可与『疯狂猜球服务号』客服协商解决。
+                    </div>
+                    <div class="intro_title">2.奖品说明</div>
+                    <div class="intro_text">
+                        请您在抽兑换商品后，尽快填写您的收货地址；1个月内未能填写有效地址的，我们视为弃奖，不再补派。
+                        <br> 因网络异常或者用户行为异常，部分奖品在兑换后，可能会延迟发放，进入待审核状态；我们会有工作人员在1-3个工作日完成审核确认，该确认阶段不会影响正常用户的兑换。
+                    </div>
+                    <div class="intro_title">3.其他声明</div>
+                    <div class="intro_text">
+                        如检测到您恶意使用第三方工具或者通过活动漏洞参与本活动，疯狂猜球运营团队有权回收您所有关联账号的异常所得，并追回已经造成的损失。
+                    </div>
+                    <div class="intro_title">4.用户服务</div>
+                    <div class="intro_text">
+                        如有任何疑问，请联系『疯狂猜球服务号』。
+                    </div>
+                    <!-- 说明结束 -->
             </div>
         </div>
     </div>
