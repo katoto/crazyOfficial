c848c5a28dae4659f41f6b642f90917bd4d1db79
xiezg <xiezg@500wan.com>
2018-01-12 16:09:30 +0800

    '112'

--- src/pages/chargeNew.vue
+++ src/pages/chargeNew.vue
@@ -789,11 +789,9 @@ export default {
             },
             onError (e) {
                 this.isShowCopyBox = true;
-                alert( e );
                 console.log( e.text );
-
                 if (e.text) {
-                    this.value = e.text
+                    this.copyValue = e.text
                 }
                 this.$store.dispatch('showToast', '复制失败~请手动复制')
             },
